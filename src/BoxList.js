63fe77afab46bdade2f7e4e00b73f032fe7cece5
sairina <sairina.merinotsui@gmail.com>
2020-02-28 09:12:29 -0800

    added comments about boxlist

--- src/BoxList.js
+++ src/BoxList.js
@@ -9,15 +9,19 @@ function BoxList() {
 
   //render boxes array
   //will need to iterate over boxes
+
+  //could also just make boxComponents (just the map of the array)
+  //and put it in the return at the bottom without calling function
+  //what is preferred?
   const renderBoxes = () => {
     return (
       <div className="BoxList-renderBoxes">
-        {boxes.map(({id, backgroundColor, width, height, removeBox}) => (
-          <Box key={id}
-            id = {id}
-            backgroundColor={backgroundColor}
-            width={width}
-            height={height}
+        {boxes.map(box => (
+          <Box key={box.id}
+            id = {box.id}
+            backgroundColor={box.backgroundColor}
+            width={box.width}
+            height={box.height}
             removeBox={removeBox}
           />
         ))}
